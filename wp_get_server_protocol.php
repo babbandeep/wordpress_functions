ec60d671f8b08b2598ffe1586eb44be1f3bd4306
babbandeep <babangurdaspur@gmail.com>
2017-07-16 23:29:46 +0530

    Create wp_get_server_protocol.php

---
+++ wp_get_server_protocol.php
@@ -0,0 +1,7 @@
+function wp_get_server_protocol() {
+	$protocol = $_SERVER['SERVER_PROTOCOL'];
+	if ( ! in_array( $protocol, array( 'HTTP/1.1', 'HTTP/2', 'HTTP/2.0' ) ) ) {
+		$protocol = 'HTTP/1.0';
+	}
+	return $protocol;
+}
