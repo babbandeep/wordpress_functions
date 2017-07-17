8727eac4443176409083b86c2c7b53a1a68eed61
babbandeep <babangurdaspur@gmail.com>
2017-07-17 11:11:48 +0530

    Update wp_get_server_protocol.php

--- wp_get_server_protocol.php
+++ wp_get_server_protocol.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Return the HTTP protocol sent by the server.
+ *
+ * @since 4.4.0
+ *
+ * @return string The HTTP protocol. Default: HTTP/1.0.
+ */
+
 function wp_get_server_protocol() {
 	$protocol = $_SERVER['SERVER_PROTOCOL'];
 	if ( ! in_array( $protocol, array( 'HTTP/1.1', 'HTTP/2', 'HTTP/2.0' ) ) ) {
